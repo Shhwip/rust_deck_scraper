8a292ecbd2963b8f8483068db78ddaf42387ea01
Echo Barros <diegobarros@mail.weber.edu>
2022-10-10 13:08:32 -0600

    scraping card titles, TODO remove tokens

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::{thread,time};
 
 fn match_deck_link(text: &str) -> &str {
     lazy_static! {
@@ -24,10 +25,22 @@ fn main() {
     let deck_title_selector = scraper::Selector::parse("h3.deck-wide-header>a").unwrap();
     let deck_titles = document.select(&deck_title_selector).map(|x|
     x.value().attr("href").unwrap_or("none"));
-    deck_titles
-        .zip(1..5)
-        .for_each(|(item, number)| println!("{}. {}", number, item));
-    // TODO pull out the /mtg-decks/$DECKTITLE for the next scrape
-    // let input = document.select(&deck_title_selector).next().unwrap();
-    // print!("{}", input.value().attr("href").unwrap_or("none"));
+
+    for l in deck_titles {
+        let mut decklist_url = String::from("https://tappedout.net");
+        decklist_url.push_str(l);
+        println!("{}", decklist_url);
+        let deck_response = reqwest::blocking::get(decklist_url).unwrap().text().unwrap();
+        let deck_doc = scraper::Html::parse_document(&deck_response);
+        let card_selector = scraper::Selector::parse("span.card>a").unwrap();
+        let card_titles = deck_doc.select(&card_selector).map(|x|
+            x.value().attr("data-name").unwrap_or(""));
+        let mut i = 0;
+        for card_title in card_titles {
+            println!("{}, {}", {i += 1; i}, card_title);
+        }
+        thread::sleep_ms(500);
+
+    }
+    // let decklist_page = reqwest::blocking::get("https://tappedout.net")
 }
